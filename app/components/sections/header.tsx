a48a13d91e0628393c8275bd761a998be68c52ce
Hossein <141348426+SHEKAF1234@users.noreply.github.com>
2024-11-10 10:51:30 +0330

    setting

--- app/components/sections/header.tsx
+++ app/components/sections/header.tsx
@@ -4,16 +4,21 @@ import styled from "styled-components";
 
 interface BlocksType {
   setting: {
-    headingColor?: string;
-    headingFontSize?: string;
-    headingFontWeight?: string;
-    btnBackgroundColor?: string;
-    btnTextColor?: string;
-    btnBackground?: string;
-    textHeading?: string;
+    titleColor?: string;
+    titleFontSize?: string;
+    titleFontWeight?: string;
+    imageWidth?: string;
+    imageHeight?: string;
+    imageRadius?: string;
+    itemColor?: string;
+    itemFontSize?: string;
+    itemFontWeight?: string;
+    itemHoverColor?: string;
+    backgroundColorNavbar?: string;
   };
   tilte?: string;
   imageLogo?: string;
+  imageAlt?: string;
 }
 
 interface SettingType {
@@ -21,31 +26,45 @@ interface SettingType {
   paddingBottom?: string;
   marginTop?: string;
   marginBottom?: string;
+  navbarPosition?: string;
 }
 
 interface SectionData {
   blocks: BlocksType;
   setting: SettingType;
 }
+const sectionData = nullData.sections.sectionHeader as SectionData;
 
+// Styled components
 const SectionHeader = styled.section`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  padding-top: ${sectionData.setting.paddingTop};
+  padding-bottom: ${sectionData.setting.paddingBottom};
+  margin-top: ${sectionData.setting.marginTop};
+  margin-bottom: ${sectionData.setting.marginBottom};
+  background-color: ${sectionData.blocks.setting.backgroundColorNavbar};
+  position: ${sectionData.setting.navbarPosition};
 `;
 const Logo = styled.img`
-  width: 100px;
+  width: ${sectionData.blocks.setting.imageWidth};
+  height: ${sectionData.blocks.setting.imageHeight};
+  border-radius: ${sectionData.blocks.setting.imageRadius};
+`;
+const Title = styled.h1`
+  color: ${sectionData.blocks.setting.titleColor};
+  font-size: ${sectionData.blocks.setting.titleFontSize};
+  font-weight: ${sectionData.blocks.setting.titleFontWeight};
 `;
 
-const sectionData = nullData.sections.sectionHeader as SectionData;
-
 const Header: React.FC = () => {
-  const { tilte, imageLogo } = sectionData.blocks;
+  const { tilte, imageLogo, imageAlt } = sectionData.blocks;
   return (
     <SectionHeader>
       <h1>
-        <Logo src={imageLogo} alt="logo" />
+        <Logo src={imageLogo} alt={imageAlt} />
         {<span>{tilte}</span>}
       </h1>
     </SectionHeader>
