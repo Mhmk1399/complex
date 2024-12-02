2e2472bfed8f919de52277f67ae3f3fee172e6bd
Hossein <141348426+SHEKAF1234@users.noreply.github.com>
2024-12-02 18:29:16 +0330

    Update page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -70,6 +70,23 @@ export default function AuthPage() {
               />
             </div>
           )}
+          {isLogin && (
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="name"
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                value={formData.name}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              />
+            </div>
+          )}
+          
 
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
